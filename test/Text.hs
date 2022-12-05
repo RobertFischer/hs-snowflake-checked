57271941aca9489255ccdd3bb5d4c226764b17b5
Robert Fischer <robert@subtlemedical.com>
2022-12-05 10:10:12 -0500

    test(monotonically-increasing): add a text and word variation of the monotonically increasing test

--- test/Text.hs
+++ test/Text.hs
@@ -14,6 +14,7 @@ import Data.Bits (shiftR)
 import Data.Ratio ( (%) )
 import qualified Data.Text as T
 import qualified Hedgehog.Gen           as Gen
+import qualified Hedgehog.Range         as Range
 
 tests :: IO Bool
 tests = checkParallel $$(discover)
@@ -37,3 +38,19 @@ prop_flakeToStrictTextHasRightLength = property $ do
     T.length value === hexDigitCount (snowcheckedConfigBitCount cfg)
   where
     hexDigitCount = ceiling . ( % 4 )
+
+prop_monotonicallyIncreasing :: Property
+prop_monotonicallyIncreasing = property $ do
+    cfg <- forAll genConfig
+    let flakeCount = min 1024 (fromIntegral $ uniqueFlakeCount cfg)
+    lst <- forAll $ Gen.list (Range.linear 2 flakeCount) (return ())
+    nodeId <- forAll genWord256
+    flakeGen <- newSnowcheckedGen cfg nodeId
+    resultLst <- mapM (\_ -> nextFlake flakeGen) lst
+    checkResults $ unBase16 . fromFlake @(Base16 Text) <$> resultLst
+  where
+    checkResults [] = success
+    checkResults [_] = success
+    checkResults (a:b:rest) = do
+      diff a (<) b
+      checkResults (b:rest)
--- test/Word32.hs
+++ test/Word32.hs
@@ -9,15 +9,35 @@ import           Data.Snowchecked.Encoding.Integral
 import           Data.Word
 import           Gens
 import           Hedgehog
+import qualified Hedgehog.Gen           as Gen
+import qualified Hedgehog.Range         as Range
 
 tests :: IO Bool
 tests = checkParallel $$(discover)
 
 prop_flakeToWord32ToFlake :: Property
 prop_flakeToWord32ToFlake = withDiscards (999999999 :: DiscardLimit) . property $ do
-		cfg <- forAll genConfig
-		unless (snowcheckedConfigBitCount cfg <= 32) discard
-		flake <- forAllFlake' cfg
-		let value = fromFlake @Word32 flake
-		let result = parseFlake cfg value
-		result === Just flake
+    cfg <- forAll genConfig
+    unless (snowcheckedConfigBitCount cfg <= 32) discard
+    flake <- forAllFlake' cfg
+    let value = fromFlake @Word32 flake
+    let result = parseFlake cfg value
+    result === Just flake
+
+prop_monotonicallyIncreasing :: Property
+prop_monotonicallyIncreasing = withDiscards (999999999 :: DiscardLimit) . property $ do
+    cfg <- forAll genConfig
+    unless (snowcheckedConfigBitCount cfg <= 32) discard
+    unless (confTimeBits cfg >= 16) discard
+    let flakeCount = min 1024 (fromIntegral $ uniqueFlakeCount cfg)
+    lst <- forAll $ Gen.list (Range.linear 2 flakeCount) (return ())
+    nodeId <- forAll genWord256
+    flakeGen <- newSnowcheckedGen cfg nodeId
+    resultLst <- mapM (\_ -> nextFlake flakeGen) lst
+    checkResults $ fromFlake @Word32 <$> resultLst
+  where
+    checkResults [] = success
+    checkResults [_] = success
+    checkResults (a:b:rest) = do
+      diff a (<) b
+      checkResults (b:rest)
